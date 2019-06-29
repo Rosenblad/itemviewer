e808b2567a0fac4db48ed642bd7a8ae9f912af26
Sebastian Nordqvist <nordqvist.sebastian@gmail.com>
2019-06-29 22:15:16 +0200

    add redux devtools enhancer to store

--- src/state/store/configureStore.ts
+++ src/state/store/configureStore.ts
@@ -1,9 +1,23 @@
-import { createStore, Store } from 'redux';
+/* eslint-disable no-underscore-dangle */
+import { createStore, Store, DeepPartial } from 'redux';
 import rootReducer from '../rootReducer';
 import { AppState } from '../types';
 
-export default function configureStore(initialState?: AppState): Store {
-  const store = createStore(rootReducer, initialState);
+// eslint-disable-next-line @typescript-eslint/no-namespace
+declare namespace window {
+  const __REDUX_DEVTOOLS_EXTENSION__: any;
+  const __REDUX_DEVTOOLS_EXTENSION_COMPOSE__: any;
+}
+
+export default function configureStore(
+  initialState?: DeepPartial<AppState>,
+): Store {
+  const store = createStore(
+    rootReducer,
+    initialState,
+    window.__REDUX_DEVTOOLS_EXTENSION__ &&
+      window.__REDUX_DEVTOOLS_EXTENSION__(),
+  );
 
   if (module.hot) {
     module.hot.accept(
