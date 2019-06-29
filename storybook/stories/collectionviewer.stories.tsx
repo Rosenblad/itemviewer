ed14123e17d86a7a2d4a018eee248b0865f04c0f
Sebastian Nordqvist <nordqvist.sebastian@gmail.com>
2019-06-29 22:00:57 +0200

    remove storybook

--- storybook/stories/collectionviewer.stories.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-// @ts-ignore
-import { ListItemActions } from 'collectionviewer';
-
-const stories = storiesOf('Collection Viewer', module);
-
-stories.add('ListItemActions', () => <ListItemActions />);
